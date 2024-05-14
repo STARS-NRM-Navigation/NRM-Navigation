2e5fdf65306aed90cdb76b60e7c8fb5fdc8c238f
Rachel Chiong <s4698038@student.uq.edu.au>
2024-05-15 09:20:10 +1000

    Add diagnostic test for LED

--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -13,94 +13,36 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+#include <stdint.h> // uint8_t
+#include <Arduino.h>
+
 // Custom Libraries
-#include "../main/tof.h"
-#include "../main/led.h"
+#include "led.h"
 
-/* === MQTT ================================================================ */
 
-WiFiClient mqtt_client;
-PubSubClient client(mqtt_client);
+// List of features of the AUnit library that we want to examine.
+#define FEATURE_BASELINE 0
+#define FEATURE_AUNIT 1
+#define FEATURE_AUNIT_VERBOSE 2
 
-/**
- * mqtt_callback()
- * @brief Callback function to retrieve mqtt data from specified topic and prints
- *      to the M5Core2 LCD screen
-*/
-void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-    M5.Lcd.printf("[%s]\n", topic);
-
-    for (int i = 0; i < length; i++) {
-        M5.Lcd.print((char)payload[i]);
-    }
-
-    M5.Lcd.println();
-}
-
-/**
- * wifi_reConnect()
- * @brief Reconnects the M5Core2 to the specified WiFi network
- * @ref CSSE4011 Lecture 4
-*/
-void wifi_reConnect() {
-
-    while (!client.connected()) {
-
-        M5.Lcd.print("Attempting MQTT connection...");
-
-        // Connect to the wifi client and publish to the specified topic
-        if (client.connect(String(NodeID).c_str())) {
-            M5.Lcd.printf("\nSuccessfully connected\n");
-            // Once connected, publish an announcement to the topic.
-
-            client.publish(mqtt_topic, "M5Core2 connected");
-            // ... and resubscribe.
-            client.subscribe(mqtt_topic);
-        } else {
-            // If unable to connect, try again in 5 seconds
-            M5.Lcd.print("failed, rc=");
-            M5.Lcd.print(client.state());
-            M5.Lcd.println("try again in 5 seconds");
-            delay(5000);
-        }
-    }
-}
-
-/**
- * wifi_init()
- * @brief Initialise the wifi connection to the MQTT broker and display connection
- *      status on the M5Core2 LCD screen
- * @ref CSSE4011 Lecture 4
-*/
-void wifi_init() {
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    M5.Lcd.setCursor(0, 50);
-    M5.Lcd.println("Trying to connect to: ");
-    M5.Lcd.println(ssid);
-
-    // Retry connection every 200ms
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(200);
-        M5.Lcd.print("*");
-    }
-    // Connected. Report configuration
-    M5.Lcd.println();
-    M5.Lcd.print("IP address: ");
-    IPAddress ip = WiFi.localIP();
-    M5.Lcd.println(ip);
-    NodeID = ip[3];
-    M5.Lcd.print("Node ID: ");
-    M5.Lcd.println(ip[3]);
-}
+int ledDiagnosticArray[6] = {LED_DANGER - 100, LED_DANGER - 100, LED_WARNING + 100, LED_WARNING + 100, LED_OK + 100, LED_OK + 100};
 
 /* === Functions =========================================================== */
 
 void setup() {
-  wifi_init();
+
+//   #if ! defined(EPOXY_DUINO)
+//   delay(1000); // wait for stability on some boards to prevent garbage Serial
+// #endif
+//   Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+//   while(!Serial); // for the Arduino Leonardo/Micro only
+// #if defined(EPOXY_DUINO)
+//   Serial.setLineModeUnix();
+// #endif
+  LED_init();
+  LED_processing(0, ledDiagnosticArray);
 }
 
 void loop() {
-  //
+  // aunit::TestRunner::run();
 }
